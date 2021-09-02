54057326d47aa420e650970e926a03923045bc22
josuerojasq <josuerojasq@gmail.com>
2021-09-02 14:56:30 -0400

    Realizamos el reto de validar y aplicar un cupón de descuento en el taller 2

--- porcentajesDescuentos/descuentos.js
+++ porcentajesDescuentos/descuentos.js
@@ -31,3 +31,79 @@ function onClickButtonPriceDiscount() {
     const resultPrice = document.getElementById("resultPrice");
     resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
+
+/** Aplicacion de Cupones
+const coupons = [
+    "JoeRojas",
+    "Lucianita",
+    "CarlitaCamila",
+];*/
+/** Array de cupones junto con sus descuentos */
+const coupons = [
+    {
+        name: "JoeRojas",
+        discount: 15,
+    },
+    {
+        name: "Lucianita",
+        discount: 30,
+    },
+    {
+        name: "CarlitaCamila",
+        discount: 25,
+    },
+];
+
+function onClickButtonPriceCoupon() {
+    const inputPrice = document.getElementById("inputPriceCoupon");
+    const inputCoupon = document.getElementById("inputCoupon");
+    const priceValue = inputPrice.value;
+    const couponValue = inputCoupon.value;
+
+    let descuento = 0;
+
+    /* switch (couponValue) {
+        case "JoeRojas":
+            descuento = 15;
+            break;
+        case "Lucianita":
+            descuento = 30;
+            break;
+        case "CarlitaCamila":
+            descuento = 25;
+            break;
+    } */
+    /** Otra alternativa al switch con array.includes
+     * nos permite validar si un elemento ha sido almacenado en el array.*/
+    /* if (!coupons.includes(couponValue)) {
+        alert("El cupón \"" + couponValue + "\" no es válido");
+    } else if (couponValue === "JoeRojas") {
+        descuento = 15;
+    } else if (couponValue === "Lucianita") {
+        descuento = 30;
+    } else if (couponValue === "CarlitaCamila"){
+        descuento = 25;
+    } */
+    /** Alternativa 3 con el array de dos valores */
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón \"" + couponValue + "\" no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultPrice = document.getElementById("resultPriceCoupon");
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
+
+    /* Esta parte la ingresamos en el else del ultimo ejemplo
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultPrice = document.getElementById("resultPriceCoupon");
+    resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento; */
+}
