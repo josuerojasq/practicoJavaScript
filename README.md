3e8ffe5c01cfffb545ef0f4999647bb953f6e7fc
josuerojasq <josuerojasq@gmail.com>
2021-09-02 08:54:18 -0400

    Taller 2: Porcentajes y Descuentos - Estructura HTML y logica en JavaScript (innerHTML)

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 - Segundo paso: implementar las formulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JavaScript con HTML
+
+## Taller #2: porcentajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: interectauar entre HTML y JavaScript
---
+++ porcentajesDescuentos/descuentos.js
@@ -0,0 +1,33 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+const porcentajePrecioConDescuento = 100 - descuento;
+const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+
+// console.log("El precio original es " + precioOriginal);
+// Otra manera de usar el console.log()
+console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento
+});
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio con descuento son $" + precioConDescuento;
+}
